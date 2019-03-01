66cf29fed83291b295a1c5b2fb1b80a942622fb1
siraphob-r <siraphob.r@linecorp.com>
2019-03-01 23:43:10 +0700

    remove token

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const fastify = require('fastify')({
   logger: true,
 });
 const axios = require('axios');
-// const TOKEN = 'xoxp-3469903797-557756360789-558507623280-def517e9d9cbb80b9e2070f8c12768fc' // thick_of_trick
-// const TOKEN = 'xoxp-3469903797-3469903803-555598613937-c359a44fa5dc4f6cf822524bb2303936' // trickpeeraze
 const CHANNEL     = '#test_github';
 const GITHUB_USER = 'trickpeeraze';
 
 const userGitMapWithSlack = {
   trickpeeraze: {
     username: 'trickpeeraze',
-    token: 'xoxp-3469903797-3469903803-555598613937-c359a44fa5dc4f6cf822524bb2303936',
+    token: '',
   },
 };
 
