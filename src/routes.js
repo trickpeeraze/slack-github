459ff4aab9686f6a53b2ff2e4888b8e922a2462c
siraphob-r <siraphob.r@linecorp.com>
2019-03-24 01:00:54 +0700

    add necleo config and new route`/health`

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
-const homeHandler = require("./handlers/home");
-const infoHandler = require("./handlers/info");
-const channelHandler = require("./handlers/channel");
-const authHandler = require("./handlers/auth");
+const homeHandler = require('./handlers/home');
+const infoHandler = require('./handlers/info');
+const channelHandler = require('./handlers/channel');
+const authHandler = require('./handlers/auth');
 
 module.exports = function(server, opts, next) {
-  server.get("/", homeHandler);
+  server.get('/', homeHandler);
 
-  server.post("/update-info", infoHandler);
+  server.post('/update-info', infoHandler);
 
-  server.post("/channel/:channelId", channelHandler);
+  server.post('/channel/:channelId', channelHandler);
 
-  server.get("/authorize", authHandler.authorize);
+  server.get('/authorize', authHandler.authorize);
 
-  server.get("/authorized", authHandler.authorized);
+  server.get('/authorized', authHandler.authorized);
+
+  server.get('/health', (req, reply) => {
+    reply.send('ok');
+  });
 
   next();
 };
