44f8ed2664dd3e4dac446f8b6b94a345271d4306
siraphob-r <siraphob.r@linecorp.com>
2019-03-25 12:56:31 +0700

    fix network cannot resolve inside docker

--- src/core/start.js
+++ src/core/start.js
@@ -1,9 +1,9 @@
 module.exports = async function(server, port) {
-  server.log.info("Loading database..");
-  server.log.info("Server starting..");
+  server.log.info('Loading database..');
+  server.log.info('Server starting..');
 
   try {
-    await server.listen(port);
+    await server.listen(port, '0.0.0.0');
   } catch (err) {
     server.log.error(err);
     process.exit(1);
