7e4527c41ac45430ceca2099b26e4325caf46206
siraphob-r <siraphob.r@linecorp.com>
2019-08-15 10:56:47 +0700

    add PR submitted body in case someone say also say somthing

--- src/slack/pullrequestreview/index.js
+++ src/slack/pullrequestreview/index.js
@@ -1,3 +1,5 @@
+const slackifyMarkdown = require('slackify-markdown');
+
 const actions = {
   submitted({ review, pull_request: pr }) {
     const getIconEmoji = name =>
@@ -22,22 +24,31 @@ const actions = {
 
     if (!stateObj) return null;
 
+    const attachments = [
+      {
+        color: stateObj.color,
+        title: `${pr.title} (#${pr.number})`,
+        title_link: pr.html_url,
+        footer: `${review.user.login} ${stateObj.action}・${
+          pr.head.repo.name
+        }`,
+        footer_icon: `https://api.adorable.io/avatars/16/${
+          review.user.login
+        }.png`,
+      },
+    ];
+
+    if (review.body) {
+      attachments.push({
+        color: '#F8F8F8',
+        text: `:talk: ${slackifyMarkdown(review.body)}`,
+      });
+    }
+
     return {
       username: `PR ${stateObj.name}`,
       icon_emoji: stateObj.image,
-      attachments: [
-        {
-          color: stateObj.color,
-          title: `${pr.title} (#${pr.number})`,
-          title_link: pr.html_url,
-          footer: `${review.user.login} ${stateObj.action}・${
-            pr.head.repo.name
-          }`,
-          footer_icon: `https://api.adorable.io/avatars/16/${
-            review.user.login
-          }.png`,
-        },
-      ],
+      attachments,
     };
   },
   edited() {
